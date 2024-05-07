c2bc56d2df27bfae880b5d5ecb91d8b5bea4d25c
Adwaith Nair <adwaith1310@gmail.com>
2024-05-07 20:03:16 +0530

    added deployment container

---
+++ src/Components/Stats/DeploymentDuration/DeploymentDuration.jsx
@@ -0,0 +1,19 @@
+import deployedIMG from "../../../assets/deployed.svg";
+import deploymentStyles from "./deployment.module.css";
+
+export default function DeploymentDuration() {
+  return (
+    <div className={deploymentStyles.deployedContainer}>
+      <img
+        src={deployedIMG}
+        alt="deployed-img"
+        className={deploymentStyles.image}
+      ></img>
+      <div className={deploymentStyles.deployedText}>
+        <h2 className={deploymentStyles.days}>10 DAYS</h2>
+        <br></br>
+        <p className={deploymentStyles.staff}>Staff Deployment</p>
+      </div>
+    </div>
+  );
+}
--- src/Components/Stats/Stats.jsx
+++ src/Components/Stats/Stats.jsx
@@ -1,11 +1,13 @@
 import GirlComponent from "./GirlComponent/GirlComponent";
 import ProjectExecution from "./ProjectExecution/ProjectExecution";
+import DeploymentDuration from "./DeploymentDuration/DeploymentDuration";
 
 export default function Stats() {
   return (
     <>
       <GirlComponent />
       <ProjectExecution />
+      <DeploymentDuration />
     </>
   );
 }
